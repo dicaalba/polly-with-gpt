a2d92126c4699612160763cb0fb1225fe9bbdbb6
Kevin RodBe <kevin.rodriguez992@gmail.com>
2023-08-15 20:08:21 -0500

    style: run prettier + update deps

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 import { Authenticator } from '@aws-amplify/ui-react';
-import "./index.css";
+import './index.css';
 
-import { useVoice } from "./useVoice";
-import Mic from "./microphone-black-shape.svg";
-import { useAnswerFetch } from "./useAnswerFetch";
+import { useVoice } from './useVoice';
+import Mic from './microphone-black-shape.svg';
+import { useAnswerFetch } from './useAnswerFetch';
 
 const App = () => {
   const { text, isListening, listen, voiceSupported } = useVoice();
   const { answer, isFetchingAnswer, fetchAnswer } = useAnswerFetch();
 
   useEffect(() => {
-    if (text !== "") {
+    if (text !== '') {
       fetchAnswer(text);
     }
   }, [text]);
@@ -19,10 +19,7 @@ const App = () => {
   if (!voiceSupported) {
     return (
       <div className="app">
-        <h1>
-          Voice recognition is not supported by your browser, please re-try with
-          a supported browser e.g. Chrome
-        </h1>
+        <h1>Voice recognition is not supported by your browser, please re-try with a supported browser e.g. Chrome</h1>
       </div>
     );
   }
@@ -30,29 +27,18 @@ const App = () => {
   return (
     <Authenticator>
       {({ signOut, user }) => (
-      <div className="app">
-        <h1>Hello {user.username}</h1>
-        <button onClick={signOut}>Sign out</button>
-        <h2>Ask to GPT</h2>
-        <h3>Click the Mic and say your question</h3>
-        <div>
-          <img
-            className={`microphone ${isListening && "isListening"}`}
-            src={Mic}
-            alt="microphone"
-            onClick={listen}
-          />
+        <div className="app">
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+          <h2>Ask to GPT</h2>
+          <h3>Click the Mic and say your question</h3>
+          <div>
+            <img className={`microphone ${isListening && 'isListening'}`} src={Mic} alt="microphone" onClick={listen} />
+          </div>
+          <p>{text}</p>
+          {isFetchingAnswer ? 'Consultando a GPT....' : <ul>{answer}</ul>}
         </div>
-        <p>{text}</p>
-        {isFetchingAnswer ? (
-          "Consultando a GPT...."
-        ) : (
-          <ul>
-            {answer}
-          </ul>
-        )}
-      </div>
-       )}
+      )}
     </Authenticator>
   );
 };
